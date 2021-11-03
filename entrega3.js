a5bc2e31da20bc7e739ced9efbc5af8e6005eaa6
Nedrox <henryjavierco@gmail.com>
2021-11-03 16:21:32 -0500

    init

---
+++ entrega3.js
@@ -0,0 +1,10 @@
+/* Declaración de Variables */
+
+let nota = parseInt(prompt("Ingrese la nota de su Proyecto Final (0-10)"));
+
+//Condicional
+if (nota >= 7) {
+    alert("¡Aprobó su Proyecto Final!");
+} else {
+    alert("Su Proyecto no fue aprobado");
+}
