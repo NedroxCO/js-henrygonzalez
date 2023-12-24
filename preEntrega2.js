2a906b712d80484ba376cecf7315e6ddc50ac9d3
Nedrox <henryjavierco@gmail.com>
2023-12-23 22:13:01 -0500

    Update preEntrega2.js
    update1

--- preEntrega2.js
+++ preEntrega2.js
@@ -5,3 +5,19 @@ Comisión: Javascript #49820
 //Simulador Cajero | ATM
 
 //Profe, entrego el link que posteriormente voy a actualizar, ya que he estado muy enfermo y no he tenido tiempo de realizar la entrega en tiempo y forma, si lee esto y no hay código abajo, por favor déjeme de último o me puede escribir directamente, si hay código pues está listo. ¡Gracias!
+
+//Declaración de Variables
+var cuenta = 1000;
+
+//Creacion de funciones
+const depositar = (cuenta) => {
+    return saldo;
+};
+
+//Objeto contenedor de Cuentas
+
+//Inicio de sesión
+
+let nombre = prompt("Por favor ingrese el nombre de la cuenta a consultar");
+
+//
