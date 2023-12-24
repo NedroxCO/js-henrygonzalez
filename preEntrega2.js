705c139e6ca5b71ba24e7a94e375521cd9111ae7
Nedrox <henryjavierco@gmail.com>
2023-12-23 21:32:52 -0500

    main
    Comment

--- preEntrega2.js
+++ preEntrega2.js
@@ -3,3 +3,5 @@ Nombre: Henry Javier González Soto
 Comisión: Javascript #49820
 */
 //Simulador Cajero | ATM
+
+//Profe, entrego el link que posteriormente voy a actualizar, ya que he estado muy enfermo y no he tenido tiempo de realizar la entrega en tiempo y forma, si lee esto y no hay código abajo, por favor déjeme de último o me puede escribir directamente, si hay código pues está listo. ¡Gracias!
